d6a5fa538c299c9535485deaabf9bcb1482fb5ce
Shashwat Kapoor <99074815+shagithubrit@users.noreply.github.com>
2023-06-07 09:25:27 +0530

    Update models.py

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Article(models.Model):
+    title=models.CharField(max_length=20)
+    slug=models.SlugField()
+    body=models.TextField()
+    date=models.DateTimeField(auto_now_add=True)
+     #add in thumbnail
+    thumb=models.ImageField(default='default.png',blank=True)
+     #add in author later 
+    author=models.ForeignKey(User,default=None ,  on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def snippet(self):
+        return self.body[:50]+'...'
+       
