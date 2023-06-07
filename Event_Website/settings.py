cf79c70fee426b7f74f3a9464cb43458182d0dd5
Ayush varshney <ayushvarshney42003@gmail.com>
2023-06-07 11:42:05 +0530

    Resolve issue for src and Event_Website

--- Event_Website/settings.py
+++ Event_Website/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'SECRET_KEY'
+EMAIL_HOST_PASSWORD = 'SECRET_PASSWORD'
