4ed011141305b49a4eaf80acc5094d399dedee05
Shashwat Kapoor <99074815+shagithubrit@users.noreply.github.com>
2023-06-07 09:27:21 +0530

    Update urls.py

--- articles/urls.py
+++ articles/urls.py
@@ -1,1 +1,14 @@
+from django.urls import re_path as url
+# from django.urls import path
+from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+app_name='articles'
+
+urlpatterns = [ 
+     url(r'^$', views.article_list, name='list'),
+     url(r'^create/$', views.article_create, name='create'),
+     url(r'^(?P<slug>[\w-]+)/$', views.article_details, name='detail'),
+
+]
+
 
